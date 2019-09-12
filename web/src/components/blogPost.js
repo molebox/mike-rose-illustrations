35b451fcdac718c555e11a62f182ba42f457288d
HAINES-DESKTOP\richa <richardkhaines@hotmail.co.uk>
2019-09-12 20:44:05 +0200

    blog post page

--- web/src/components/blogPost.js
+++ web/src/components/blogPost.js
@@ -10,3 +10,36 @@ import { useStaticQuery } from 'gatsby';
 import BasePortableText from '@sanity/block-content-to-react'
 
 // check https://github.com/sanity-io/sanity-template-gatsby-blog/blob/master/template/web/src/components/blog-post.js
+
+const Card = styled.div`
+	background: #f1f1f1;
+	padding: 2rem;
+	-webkit-box-shadow: 4px 7px 16px -5px rgba(0, 0, 0, 0.75);
+	-moz-box-shadow: 4px 7px 16px -5px rgba(0, 0, 0, 0.75);
+	box-shadow: 4px 7px 16px -5px rgba(0, 0, 0, 0.75);
+	border-radius: 0.4em;
+
+	& > div {
+		padding: 1rem;
+	}
+
+	& h4 {
+		margin-bottom: 2rem;
+	}
+
+	@media (max-width: 700px) {
+		display: block;
+	}
+`;
+
+const BlogPost = () => {
+    const {_rawBody, title, mainImage, publishedAt} = props
+
+    return (
+        <Card>
+            <h1>{title}</h1>
+        </Card>
+    );
+}
+
+export default BlogPost;
