c60de146c04021fd1ac581a877edf4eba2ed48b8
HAINES-DESKTOP\richa <richardkhaines@hotmail.co.uk>
2019-09-13 23:00:30 +0200

    remove imports

--- web/src/components/blogPost.js
+++ web/src/components/blogPost.js
@@ -1,13 +1,6 @@
 /** @jsx jsx */
 import { jsx } from 'theme-ui';
 import styled from '@emotion/styled';
-import Layout from '../components/layout';
-import Main from './../components/main';
-import Sidebar from '../components/sidebar/Sidebar';
-import Img from 'gatsby-image';
-import { graphql } from 'gatsby';
-import { useStaticQuery } from 'gatsby';
-import BasePortableText from '@sanity/block-content-to-react'
 
 // check https://github.com/sanity-io/sanity-template-gatsby-blog/blob/master/template/web/src/components/blog-post.js
 
